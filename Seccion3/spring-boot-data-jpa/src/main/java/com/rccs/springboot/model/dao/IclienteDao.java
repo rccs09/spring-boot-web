2e457a04a322a255450f7c2becd370c71f785e87
rccs09 <rccs09@gmail.com>
2019-12-01 00:11:12 -0500

    70. Paginador desde cero - Usando PagingAndSortingRepository - Parte I

--- Seccion3/spring-boot-data-jpa/src/main/java/com/rccs/springboot/model/dao/IclienteDao.java
+++ Seccion3/spring-boot-data-jpa/src/main/java/com/rccs/springboot/model/dao/IclienteDao.java
@@ -1,9 +1,9 @@
 package com.rccs.springboot.model.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.rccs.springboot.model.entity.Cliente;
 
-public interface IclienteDao extends CrudRepository<Cliente, Long> {
+public interface IclienteDao extends PagingAndSortingRepository<Cliente, Long> {
 
 }
--- Seccion3/spring-boot-data-jpa/src/main/java/com/rccs/springboot/model/service/IClienteService.java
+++ Seccion3/spring-boot-data-jpa/src/main/java/com/rccs/springboot/model/service/IClienteService.java
@@ -2,6 +2,9 @@ package com.rccs.springboot.model.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.rccs.springboot.model.entity.Cliente;
 
 public interface IClienteService {
@@ -12,4 +15,6 @@ public interface IClienteService {
 	Cliente findById(Long id);
 	
 	public void delete(Long id);
+	
+	Page<Cliente> findAll(Pageable pageable);
 }
